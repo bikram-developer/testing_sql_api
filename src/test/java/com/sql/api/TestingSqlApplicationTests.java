641eae88faa584c2fce70eb283d60a2dcf25fc52
bikram-developer <bikram.developers@gmail.com>
2024-01-19 00:27:11 +0530

    docker added

--- src/test/java/com/sql/api/TestingSqlApplicationTests.java
+++ src/test/java/com/sql/api/TestingSqlApplicationTests.java
@@ -1,13 +1,18 @@
-package com.sql.api;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TestingSqlApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.sql.api;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TestingSqlApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
+//FROM eclipse-temurin:17-jdk-alpine
+//VOLUME /tmp
+//COPY target/*.jar hello.jar
+//ENTRYPOINT ["java","-jar","/hello.jar"]
+//EXPOSE 8080
