c74c498988e2a44a713372e4fd0f1fe9c8d8668a
bikram-developer <bikram.developers@gmail.com>
2024-01-19 00:38:08 +0530

    without database

--- src/main/java/com/sql/api/Controller/TestController.java
+++ src/main/java/com/sql/api/Controller/TestController.java
@@ -8,26 +8,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sql.api.entity.TestEntity;
-import com.sql.api.repo.TestRepo;
+//import com.sql.api.repo.TestRepo;
 
 @RestController
 @RequestMapping("api/v-1")
 public class TestController {
 
-	@Autowired private TestRepo repo;
+//	@Autowired private TestRepo repo;
 	
 	@GetMapping("hello")
 	public String hello() {
 		return "hello world";
 	}
 	
-	@GetMapping("getall")
-	public List<TestEntity> getName() {
-		try {
-			return repo.findAll();
-		} catch (Exception e) {
-			return null;
-		}
-	}
+//	@GetMapping("getall")
+//	public List<TestEntity> getName() {
+//		try {
+//			return repo.findAll();
+//		} catch (Exception e) {
+//			return null;
+//		}
+//	}
 	
 }
--- src/main/java/com/sql/api/entity/TestEntity.java
+++ src/main/java/com/sql/api/entity/TestEntity.java
@@ -1,17 +1,8 @@
 package com.sql.api.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
-@Entity
-@Table(name ="test_table")
 public class TestEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	private String name;
