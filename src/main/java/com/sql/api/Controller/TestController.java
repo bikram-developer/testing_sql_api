818ed64903dedc9048879cd6bb90c710e3b23404
bikram-developer <bikram.developers@gmail.com>
2024-01-20 00:28:10 +0530

    database connected

--- src/main/java/com/sql/api/Controller/TestController.java
+++ src/main/java/com/sql/api/Controller/TestController.java
@@ -1,35 +1,52 @@
 package com.sql.api.Controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //import com.sql.api.repo.TestRepo;
+
+import com.sql.api.entity.TestEntity;
+import com.sql.api.repo.TestRepo;
 
 @RestController
 @RequestMapping("api/v-1")
 public class TestController {
 
-//	@Autowired private TestRepo repo;
+	@Autowired private TestRepo repo;
 	
 	@GetMapping("hello")
 	public String hello() {
 		return "hello world";
 	}
 	
-//	@GetMapping("getall")
-//	public List<TestEntity> getName() {
-//		try {
-//			return repo.findAll();
-//		} catch (Exception e) {
-//			return null;
-//		}
-//	}
-	@Value("${server.port}")
-    private String serverPort;
-
-    @GetMapping("/getPort")
-    public String getPort() {
-        return "Server is running on port: " + serverPort;
-    }	
+	@GetMapping("getall")
+	public List<TestEntity> getName() {
+		try {
+			return repo.findAll();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	@PostMapping("add")
+	public TestEntity add(@RequestBody TestEntity entity) {
+		try {
+			return repo.save(entity);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	@GetMapping("get/{id}")
+	public TestEntity getById(@PathVariable int id) {
+		try {
+			return repo.findById(id).orElse(null);
+		} catch (Exception e) {
+			return null;
+		}
+	}
 }
--- src/main/java/com/sql/api/entity/TestEntity.java
+++ src/main/java/com/sql/api/entity/TestEntity.java
@@ -1,8 +1,14 @@
 package com.sql.api.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class TestEntity {
 
+	@Id
 	private int id;
 	
 	private String name;
